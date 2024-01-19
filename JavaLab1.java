f2bef4cb431337859c6f7141337e84e7e0d9f9df
RachelCVerastique <rachel.c.verastique@gmail.com>
2024-01-19 15:26:59 -0600

    Java lab 1

---
+++ JavaLab1.java
@@ -0,0 +1,43 @@
+public class JavaLab1 {
+
+    public static void main(String[] args){
+
+        System.out.println();
+        System.out.println("Hello World! This is Rachel!");
+        System.out.println();
+
+
+        System.out.println("//// \\\\\\\\ **** //// \\\\\\\\ ");
+        System.out.println();
+
+
+        System.out.println(" SSS         A           CCCC");
+        System.out.println("S           A A         C");
+        System.out.println(" SSS       AAAAA        C");
+        System.out.println("    S     A     A       C");
+        System.out.println(" SSS     A       A       CCCC");
+        System.out.println();
+
+
+        System.out.println("+------------+");
+        System.out.println("|            |");
+        System.out.println("|            |");
+        System.out.println("|            |");
+        System.out.println("+------------+");
+        System.out.println();
+
+
+
+        System.out.println("RRRR      CCCC    V       V");
+        System.out.println("R   R    C         V     V");
+        System.out.println("RRRR     C          V   V");
+        System.out.println("R   R    C           V V");
+        System.out.println("R    R    CCCC        V");
+        System.out.println();
+
+
+
+
+
+    }
+}
